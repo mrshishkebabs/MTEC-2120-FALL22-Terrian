63dddb2f810869773b0ed727fc0cbb6b5cd821bc
mrshishkebabs <xkenxparkx@gmail.com>
2022-10-14 03:10:22 -0400

    Redo

---
+++ MTEC-2120-FALL22-Terrian/Assets/Something.cs
@@ -0,0 +1,34 @@
+    // 'using' is a keyword for importing library from a namespace.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Something : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Useful single line comment
+    //
+    //
+    //
+
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
